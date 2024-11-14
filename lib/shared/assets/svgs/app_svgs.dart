a752ddb3d23e845527fa793a5e5bace546d03c22
Pablo Stefan Gomes Pereira <pablo.stefan.dev@gmail.com>
2024-11-14 03:14:35 -0300

    Add new SVG assets and update pubspec.yaml
    Added various SVG icons to the assets folder and updated `pubspec.yaml` to include `flutter_svg` dependency and new asset paths. This ensures the app can properly use and reference the new SVG assets.

---
+++ lib/shared/assets/svgs/app_svgs.dart
@@ -0,0 +1,14 @@
+enum AppSvgs {
+  ic_bolt,
+  ic_codepen,
+  ic_companies,
+  ic_cube,
+  ic_ellipse,
+  ic_energy,
+  ic_exclamation,
+  ic_location,
+  ic_logo,
+  ic_search;
+
+  String get path => "assets/svgs/$name.svg";
+}
